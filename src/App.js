d1c29a2ac5b873219660346cb656d6624a12f50c
glen-harry <glenharry84@gmail.com>
2022-11-24 19:30:41 +1100

    .env set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+const baseURL = `https://api.openweathermap.org/data/2.5/weather`;
+
+const apiKey = process.env.REACT_APP_API_KEY;
+console.log(apiKey);
+
+const units = `&units=metric`;
+const queryString = `?q=London&appid=${apiKey}${units}`;
+const apiEndpoint = baseURL + queryString;
+
+console.log(apiEndpoint);
+
+function waitForData(response) {
+  return response.json();
+}
+
+function handleData(data) {
+  let maxTemp = data.main.temp_max;
+  let minTemp = data.main.temp_min;
+  let currentTemp = data.main.temp;
+  console.log(`Current Temp: ${currentTemp}`);
+  console.log(`Max Temp: ${maxTemp}`);
+  console.log(`Min Temp: ${minTemp}`);
+}
+
+fetch(apiEndpoint).then(waitForData).then(handleData);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <input type="text" />
+      <button>Search</button>
     </div>
   );
 }
